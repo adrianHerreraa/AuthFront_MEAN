9d54fed3e15397785d27ac8dd9ab054497124fd7
AdrianHerrera_ <neomach55@gmail.com>
2021-11-26 23:42:31 -0600

    Terminado

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,29 +1,40 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegisterComponent {
-
   miFormulario: FormGroup = this.fb.group({
     name: ['Luis Adrian', [Validators.required]],
     email: ['test1@test.com', [Validators.required, Validators.email]],
     password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor( 
+  constructor(
     private fb: FormBuilder,
     private router: Router,
-  ) { }
+    private authService: AuthService
+  ) {}
 
-  registro(){
-    console.log(this.miFormulario.value);
-    this.router.navigateByUrl('/dashboard');
+  registro() {
+    const { name, email, password } = this.miFormulario.value;
+
+    this.authService.registro(name, email, password).subscribe((ok) => {
+      if (ok === true) {
+        this.router.navigateByUrl('/dashboard');
+      }else{
+        Swal.fire({
+          title: 'Error',
+          text: ok.error.msg,
+          icon: 'error',
+        });
+      }
+    });
   }
-
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -11,47 +11,87 @@ import { environment } from '../../../environments/environment';
 import { AuthResponse, Usuario } from '../interfaces/auth.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private _baseUrl: string = environment.baseUrl;
   private _user!: Usuario;
 
   get user() {
-    return {...this._user};
+    return { ...this._user };
   }
 
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
-  login( email: string, password: string ){
-    
+  registro(name: string, email: string, password: string) {
+    const url = `${this._baseUrl}/auth/new`;
+    const body = { name, email, password };
+
+    return this.http.post<AuthResponse>(url, body).pipe(
+      tap((resp) => {
+        if (resp.ok) {
+          localStorage.setItem('token', resp.token!);
+          /*this._user = {
+            name: resp.name!,
+            uid: resp.uid!,
+          };*/
+        }
+      }),
+      map((resp) => resp.ok),
+      catchError((err) => of(err))
+    );
+  }
+
+  login(email: string, password: string) {
     const url = `${this._baseUrl}/auth`;
     const body = { email, password };
-    
-    return this.http.post<AuthResponse>(url, body)
-      .pipe(
-        tap( resp => {
-          if( resp.ok ){
-            
-            localStorage.setItem('token', resp.token!);
 
-            this._user = {
-              name: resp.name!,
-              uid: resp.uid!,
-            };
-          }
-        }),
-        map( resp => resp.ok ),
-        catchError( err => of(err.error) )
-      )  
-  };
+    return this.http.post<AuthResponse>(url, body).pipe(
+      tap((resp) => {
+        if (resp.ok) {
+          localStorage.setItem('token', resp.token!);
 
-  validarToken(){
-    const url = `${this._baseUrl}/auth/renew`;
-    const misHeaders = new HttpHeaders().set( 'x-token', localStorage.getItem('token') || '');
-    return this.http.get(url, { headers: misHeaders });
+          /*this._user = {
+            name: resp.name!,
+            uid: resp.uid!,
+          };*/
+        }
+      }),
+      map((resp) => resp.ok),
+      catchError((err) => of(err.error))
+    );
   }
 
+  validarToken(): Observable<boolean> {
+    const url = `${this._baseUrl}/auth/renew`;
+    const misHeaders = new HttpHeaders().set(
+      'x-token',
+      localStorage.getItem('token') || ''
+    );
 
+    return this.http.get<AuthResponse>(url, { headers: misHeaders }).pipe(
+      map((resp) => {
+        console.log(resp);
+
+        localStorage.setItem('token', resp.token!);
+
+        this._user = {
+          name: resp.name!,
+          uid: resp.uid!,
+          email: resp.email!,
+        };
+
+        return resp.ok;
+      }),
+      catchError((err) => of(false))
+    );
+  }
+
+  logout() {
+    // Borra unicamente el key especificado.
+    // localStorage.removeItem('token');
+
+    // Borra todas las keys guardadas.
+    localStorage.clear();
+  }
 }
